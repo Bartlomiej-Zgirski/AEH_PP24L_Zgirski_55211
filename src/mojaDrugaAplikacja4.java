957b62bb5b640876112bc351b0f299408ba2e56a
Lab2 <zgirsba4529_aehit@students.vizja.pl>
2024-03-11 20:39:53 +0100

    Pierwszy zapis do repozytorium Lab2

---
+++ src/mojaDrugaAplikacja4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class mojaDrugaAplikacja4 {
+    public static void main(String[] args) {
+
+        double x = 20.5;
+
+        Scanner scanner = new Scanner(System.in);
+
+        var result = 365 * 24 * 60 * 60 * x;
+        System.out.println("365 * 24 * 60 * 60 * x =  " + result);
+
+
+        }
+    }
